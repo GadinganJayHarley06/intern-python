0225fdb29490a467c39ba2058641b62f39b2b68c
Jay Harley Gadingan <jayharleygadz@gmail.com>
2018-04-13 09:53:43 +0800

    python

---
+++ jay/greet.py
@@ -0,0 +1,4 @@
+def greet(yo):
+    name = raw_input("What's your name: ")
+    return yo + name
+print greet("hi! ")
---
+++ jay/sum.py
@@ -0,0 +1,7 @@
+def listsum(numList):
+    theSum = 0
+    for i in numList:
+        theSum = theSum + i
+    return theSum
+
+print(listsum([5,27,36,47,89]))
